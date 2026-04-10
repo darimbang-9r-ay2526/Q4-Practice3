3d77c4cf5d71e34bff40b683cc4af311d1901790
darimbang-9r-ay2526 <darimbang.ag@obmontessori.edu.ph>
2026-04-10 13:14:54 +0800

    a

---
+++ main.js
@@ -0,0 +1,20 @@
+function verifyCategory() {
+    var category = document.getElementById('categorya').value;
+    
+    switch (categorya) {
+        case 'A':
+            document.getElementById('display').innerHTML =
+             'Category: Clothing';
+            break;
+        case 'B':
+            document.getElementById('display').innerHTML =
+             'Category: Electronics';
+            break;
+        case 'C':
+            document.getElementById('display').innerHTML =
+             'Category: Books';
+            break;
+        default:
+            window.alert('Invalid category. Please enter A, B, or C.');
+    }
+}
